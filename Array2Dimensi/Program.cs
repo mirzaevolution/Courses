750acb672d1eb3a086204bb73a413224b58bfcb2
Mirza Ghulam Rasyid <mirzaevolution@gmail.com>
2019-01-27 16:58:21 +0700

    array2d

--- Array2Dimensi/Program.cs
+++ Array2Dimensi/Program.cs
@@ -15,8 +15,9 @@ namespace Array2Dimensi
             
 
 
-            //cara initialisasi 1
+            //cara initialisasi 1: Manual Assignment
             int[,] data = new int[2, 3];
+
             //row 0
             data[0, 0] = 1; //row 0, column 0
             data[0, 1] = 2; //row 0, column 1
@@ -27,23 +28,35 @@ namespace Array2Dimensi
             data[1, 1] = 5; //row 0, column 1
             data[1, 2] = 6; //row 0, column 2
 
-            //cara inisialisasi 2
-            int[,] data2 = new int[2, 3]
-            {
-                { 1, 2, 3 },
-                { 4, 5, 6 }
-            };
 
 
+            //cara inisialisasi 2: Direct Assignment
+            //int[,] data2 = new int[2, 3]
+            //{
+            //    { 1, 2, 3 }, ////row 0
+            //    { 4, 5, 6 }  ////row 1
+            //};
+            int[,] data2 = new int[,]
+              {
+                   { 1, 2, 3 }, //row 0
+                   { 4, 5, 6 }  //row 1
+              };
 
-            int rowLength = data2.GetLength(0);
-            int colLength = data2.GetLength(1);
+
+            
+            //mendapatkan panjang baris
+            int rowLength = data.GetLength(0);
+
+            //mendapatkan panjang kolom
+            int columnLength = data.GetLength(1);
+
+
 
             for(int rowIndex = 0; rowIndex < rowLength; rowIndex++)
             {
-                for(int colIndex=0; colIndex < colLength; colIndex++)
+                for(int colIndex=0; colIndex < columnLength; colIndex++)
                 {
-                    Console.Write("{0} ", data2[rowIndex, colIndex]);
+                    Console.Write("{0} ", data[rowIndex, colIndex]);
                 }
                 Console.WriteLine();
             }
